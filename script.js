2e951b24c056cf05e0c6e584182832ec5c433e1f
Kumar Aryan <karyan99gk@gmail.com>
2026-01-28 22:55:17 +0600

    improved ui and implemented functionality

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ const container = document.querySelector(".container");
 openBtn.addEventListener("click", () => {
     dialog.showModal();
 });
+const gameslib=[];
+
+class Game{
+    constructor(name,url,completed){
+        this.id=crypto.randomUUID();
+        this.name=name;
+        this.url=url;
+        this.completed=completed;
+    }
+}
 
 dialog.addEventListener("close", () => {
     if (dialog.returnValue === "confirm") {
@@ -15,11 +25,13 @@ dialog.addEventListener("close", () => {
         const completed = data.get("played") === "on";
         console.log({ name, url, completed });
         form.reset();
-        adddiv(name, url, completed);
+        const newgame=new Game(name,url,completed);
+        gameslib.push(newgame);
+        adddiv(newgame);
     }
 });
 
-function adddiv(name, url, completed) {
+function adddiv(newgame) {
     const card = document.createElement("div");
     card.className = "card";
 
@@ -27,20 +39,34 @@ function adddiv(name, url, completed) {
     imageDiv.className = "image";
 
     const img = document.createElement("img");
-    img.src = url;
-    img.alt = name;
-
+    img.src = newgame.url;
+    img.alt = newgame.name;
+    img.height=370;
+    img.onerror = () => {img.src = "./images/placeholder.jpg"};
     const title = document.createElement("div");
     title.className = "title";
-    title.textContent = name;
+    title.textContent = newgame.name;
 
     imageDiv.appendChild(img);
     card.appendChild(imageDiv);
     card.appendChild(title);
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "delete-btn";
+    deleteBtn.innerHTML = `<span class="material-symbols-outlined">delete</span>`;
+    card.appendChild(deleteBtn);
 
-    if (completed) {
+    if (newgame.completed) {
         card.classList.add("completed");
     }
 
     container.appendChild(card);
 }
+
+container.addEventListener("click", (e) => {
+  const deleteBtn = e.target.closest(".delete-btn");
+
+  if (!deleteBtn) return;
+
+  const card = deleteBtn.closest(".card");
+  card.remove();
+});
